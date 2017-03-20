f6033bd4372a2a04f39c0bae557fee37fac08ad0
Plamen Marinov <plamen@webdiv.live>
2017-03-20 10:26:31 +0000

    cf vd and vdd fixes

--- cf/cf.php
+++ cf/cf.php
@@ -4,22 +4,29 @@ helper::uses(["common"], basename(__FILE__,".php"));
 /** The cf (Common Functions) helper class */
 class cf extends common {
 
+    static $protectFileinfoOnDumps = false;
     
-    /** final function cf::vd() - var_dump shortage - writes vars formated dump for submitted arguments.*/
+    /** Regular php `var_dump()` replacement - writes variables dump (in a pre tag), showing the own call position. */
     final static public function vd(){
         $args = func_get_args();
+
         echo "<pre>";
         foreach($args as $value){
             var_dump($value);
         }
+        
         $callers=debug_backtrace();
-        $from= isset($callers[1], $callers[1]["class"], $callers[0]) ? 
-                "Dump triggered from: "  .  $callers[1]["class"] . "::" . $callers[1]["function"] . " | " . $callers[0]["file"] . " (line: " . $callers[0]["line"] . ")" :
-                "Dump triggered from: SomeClass::Method | some-class.php (line: ?)";
-        echo "<p style=\"font-family:Tahome,sans-serif; padding-top:5px; font-size:12px; color:#888; border-top:1px dashed #888; margin:0px;\">".$from."</p></pre>";
+        
+        $index = in_array($callers[1]["function"],array("forward_static_call", "forward_static_call_array")) ? 2 : 1;
+        $class = isset($callers[$index], $callers[$index]["class"]) ? $callers[$index]["class"]."::" : null;
+        
+        $callInfo = "Dump triggered from: "  .  $class . $callers[$index]["function"] . "()";
+        $callInfo.= self::$protectFileinfoOnDumps===false ? " | " . $callers[($index-1)]["file"] . " (line: " . $callers[($index-1)]["line"] . ")" : null;
+
+        echo "</pre><p style=\"font-family:Tahoma,sans-serif; padding-top:5px; font-size:12px; color:#888; border-top:1px dashed #888; margin:0px;\">".$callInfo."</p>";
     }
 
-    /** final function cf::vdd() - var_dump shortage with die(); - writes vars formated dump for arguments. */
+    /** Regular php `die(var_dump())` replacement - dies writing variables dump (in a pre tag), showing the own call position. */
     final static public function vdd(){
         $args = func_get_args();
         if(!headers_sent()){ echo "<meta charset=\"UTF-8\">\n";}
@@ -53,7 +60,4 @@ class cf extends common {
         $ckSettings["toolbarLocation"] = "bottom";
         return $ckSettings;
     }
-
-
-
 }
